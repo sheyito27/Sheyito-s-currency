e8900fd51ce6e95f7f7ee52498162c37d46b957a
sheyito27 <46390546+sheyito27@users.noreply.github.com>
2026-08-16 00:48:52 +0200

    feat(ServerLifecycleHandler): ciclo de vida de EmbargoManager/AuctionPoolManager
    Init/shutdown junto al resto de managers, y entrega de devoluciones
    pendientes al reconectar (jugador que estaba offline cuando su
    votacion de embargo cerro).

--- src/main/java/com/sheyito/economicmaster/events/ServerLifecycleHandler.java
+++ src/main/java/com/sheyito/economicmaster/events/ServerLifecycleHandler.java
@@ -1,13 +1,16 @@
 package com.sheyito.economicmaster.events;
 
+import com.sheyito.economicmaster.auction.AuctionPoolManager;
 import com.sheyito.economicmaster.chunk.ChunkClaimManager;
 import com.sheyito.economicmaster.config.ConfigManager;
 import com.sheyito.economicmaster.dimension.DimensionUnlockManager;
 import com.sheyito.economicmaster.economy.EconomyManager;
+import com.sheyito.economicmaster.embargo.EmbargoManager;
 import com.sheyito.economicmaster.salary.SalaryManager;
 import com.sheyito.economicmaster.shop.ShopManager;
 import com.sheyito.economicmaster.subscription.SubscriptionManager;
 import com.sheyito.economicmaster.trade.TradeManager;
+import net.minecraft.server.level.ServerPlayer;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
@@ -25,6 +28,8 @@ public class ServerLifecycleHandler {
         ShopManager.init(event.getServer());
         DimensionUnlockManager.init(event.getServer());
         ChunkClaimManager.init(event.getServer());
+        EmbargoManager.init(event.getServer());
+        AuctionPoolManager.init(event.getServer());
     }
 
     @SubscribeEvent
@@ -36,12 +41,17 @@ public class ServerLifecycleHandler {
         SubscriptionManager.shutdown();
         DimensionUnlockManager.shutdown();
         ChunkClaimManager.shutdown();
+        EmbargoManager.shutdown();
+        AuctionPoolManager.shutdown();
     }
 
     @SubscribeEvent
     public void onPlayerLoggedIn(PlayerEvent.PlayerLoggedInEvent event) {
         if (EconomyManager.get() != null) {
             EconomyManager.get().trackName(event.getEntity().getUUID(), event.getEntity().getGameProfile().getName());
+        }
+        if (EmbargoManager.get() != null && event.getEntity() instanceof ServerPlayer player) {
+            EmbargoManager.get().deliverPendingReturns(player);
         }
     }
 
